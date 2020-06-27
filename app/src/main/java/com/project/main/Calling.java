f5b4b3b25c2499eeb7eda1e07911b8501b6f011d
Animesh Sinha <35229469+deniedhash@users.noreply.github.com>
2020-06-27 15:14:20 +0530

    Commenting of Calling.java

--- app/src/main/java/com/project/main/Calling.java
+++ app/src/main/java/com/project/main/Calling.java
@@ -1,6 +1,6 @@
 package com.project.main;
 
-import org.jsoup.Jsoup;
+import org.jsoup.Jsoup; //JSoup Library to Parse HTML
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -9,46 +9,52 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Calling {
+
+    //ArrayLists to store Objects
     ArrayList<String> temptitlestore = new ArrayList<>();
     ArrayList<String> tempurlstore = new ArrayList<>();
     ArrayList<String> tempimageurl = new ArrayList<>();
     ArrayList<String> mainlist = new ArrayList<String>();
     String link;
 
+
+    //Call Function to "Even After Talking To You, I still have no Idea wtf is this. Please do the needful Kanishk. Thank You"
     public void call(int sNo,String websiteUrl,String productClass, String tagForLink, String titleClass, String discountedPriceClass, String originalPriceClass, String productImageClass,String productDiscount) throws IOException {
 
-        Document doc = Jsoup.connect(websiteUrl).get();
-        Elements links = doc.getElementsByClass(productClass);
+        Document doc = Jsoup.connect(websiteUrl).get(); //JSoup Element to get HTML Data from the corresponding website
+        Elements links = doc.getElementsByClass(productClass); //JSoup Element to get HTML Data from the corresponding Class/Section
         link = websiteUrl;
 
         // 1- Flipkart
         // 2- Shopclues
         // 3- Snapdeal
         // 4- Paytm
+
+        //Loop To Initialize Variables for storing required HTML Data
         for (Element link : links) {
             String temp1 = null, temp2 = null, temp3 = null, temp4 = null, temp5 = null, temp6 = null;
             String permanent1 = null;
             Elements eltitle, elproductimage;
 
-            Elements elLink = link.getElementsByTag(tagForLink);
+            Elements elLink = link.getElementsByTag(tagForLink); //JSoup ELement to get URL of the product
 
-            if(sNo == 2){
-                eltitle = link.getElementsByTag(titleClass);
-            }else{
-                eltitle = link.getElementsByClass(titleClass);
+            if(sNo == 2){                                        //When ShopClues is being parsed,
+                eltitle = link.getElementsByTag(titleClass);     //We are getting Title of Product using HTML Tag because there is no proper class identifying the title
+            }else{                                               //When any other website is being parsed,
+                eltitle = link.getElementsByClass(titleClass);   //We are getting Title of Product using HTML Class because there is class identifying the title
             }
 
-            Elements elpricebefore = link.getElementsByClass(originalPriceClass);
+            Elements elpricebefore = link.getElementsByClass(originalPriceClass); //JSoup Element to get Original Price Data from corresponding HTML Class
 
-            Elements elpriceafter = link.getElementsByClass(discountedPriceClass);
+            Elements elpriceafter = link.getElementsByClass(discountedPriceClass); //JSoup Element to get Discounted Price Date from corresponding HTML Class
 
-            if(sNo == 2 || sNo == 4){
-                elproductimage = link.getElementsByTag(productImageClass);
-            }else {
-                elproductimage = link.getElementsByClass(productImageClass);
+            if(sNo == 2 || sNo == 4){                                         //When ShopClues and Paytm mall is being parsed,
+                elproductimage = link.getElementsByTag(productImageClass);    //We are getting URL of Product Image by HTML Tag because there is no proper class identifying the image url
+            }else {                                                           //When any other website is being Parsed,
+                elproductimage = link.getElementsByClass(productImageClass);  //We are getting URL of Product image by HTML Class because there is a proper class identifying the image url
             }
 
-            Elements discount = link.getElementsByClass(productDiscount);
+            Elements discount = link.getElementsByClass(productDiscount);  //JSoup element to parse Discount data from corresponding classes
 
 
             //product title loop
@@ -71,20 +77,24 @@ public class Calling {
                 temp4 = "Discount: " + productdiscount.text();
             }
 
+            //image loop
             for(Element elimage : elproductimage){
                 temp6 = elimage.attr("src");
             }
 
+            //produuct URL loop
             ArrayList<String> linkArray = new ArrayList<String>();
             for (Element elementLink : elLink) {
                 String MainLink = elementLink.attr("href");
                 linkArray.add(MainLink);
             }
 
+            //URL storing
             for (int j = 0; j < 1; j++) {
                 temp5 = linkArray.get(0);
             }
 
+            //if-else block for when there is no discount
             if (elpricebefore.text()==null)
             {
                 permanent1 = "\n"+ "Price :" + elpriceafter.text() + "\n" + temp4 + "\n";
